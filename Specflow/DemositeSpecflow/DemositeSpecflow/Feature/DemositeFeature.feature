fbbf3066c918eec3442d704432083262f4e9fad9
Gabrijela <gabrijela.srbalj@gmail.com>
2023-02-05 18:23:31 +0100

    Specflow task- demosite used in tutorial

---
+++ Specflow/DemositeSpecflow/DemositeSpecflow/Feature/DemositeFeature.feature
@@ -0,0 +1,16 @@
+﻿Feature: DemositeFeature
+	Login user into demosite web page
+
+@mytag
+Scenario: Login User 
+	Given I have navigated to Demosite
+	And I have entered username 
+	And I have entered password 
+	When I press login button
+	Then I should be navigated to index page
+	And I have entered inital 
+	And I have entered first name 
+	And I have entered middle name
+	When I choose female button
+	When I press save
+	Then I should stay on index page
---
+++ Specflow/DemositeSpecflow/DemositeSpecflow/StepBindings/DemositeFeatureSteps.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace DemositeSpecflow.StepBindings
+{
+    [Binding]
+    public class DemositeFeatureSteps 
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [Given(@"I have navigated to Demosite")]
+        public void GivenIHaveNavigatedToDemosite()
+        {
+            driver.Navigate().GoToUrl("https://demosite.executeautomation.com/");
+        }
+        
+        [Given(@"I have entered username")]
+        public void GivenIHaveEnteredUsername()
+        {
+            IWebElement username = driver.FindElement(By.Name("UserName"));
+            username.SendKeys("MyUsername");
+        }
+        
+        [Given(@"I have entered password")]
+        public void GivenIHaveEnteredPassword()
+        {
+            IWebElement password = driver.FindElement(By.Name("Password"));
+            password.SendKeys("MyPassword");
+        }
+        
+        [When(@"I press login button")]
+        public void WhenIPressLoginButton()
+        {
+            IWebElement btnLogin = driver.FindElement(By.Name("Login"));
+            btnLogin.Submit();
+        }
+        
+        [Then(@"I should be navigated to index page")]
+        public void ThenIShouldBeNavigatedToIndexPage()
+        {
+            Thread.Sleep(3000);
+            Assert.IsTrue(driver.Url.ToLower().Contains("index"));
+        }
+
+        [When(@"I choose female button")]
+        public void WhenIChooseFemaleButton()
+        {
+            driver.FindElement(By.Name("Female")).Click();
+        }
+
+        [When(@"I press save")]
+        public void WhenIPressSave()
+        {
+            driver.FindElement(By.Name("Save")).Click();
+        }
+
+        [Then(@"I have entered inital")]
+        public void ThenIHaveEnteredInital()
+        {
+            driver.FindElement(By.Name("Initial")).SendKeys("GS");
+        }
+
+        [Then(@"I have entered first name")]
+        public void ThenIHaveEnteredFirstName()
+        {
+            driver.FindElement(By.Name("FirstName")).SendKeys("Gabrijela");
+        }
+
+        [Then(@"I have entered middle name")]
+        public void ThenIHaveEnteredMiddleName()
+        {
+            driver.FindElement(By.Name("MiddleName")).SendKeys("Gabi");
+        }
+
+        [Then(@"I should stay on index page")]
+        public void ThenIShouldStayOnIndexPage()
+        {
+            Thread.Sleep(3000);
+            Assert.IsTrue(driver.Url.ToLower().Contains("index"));
+        }
+    }
+}
